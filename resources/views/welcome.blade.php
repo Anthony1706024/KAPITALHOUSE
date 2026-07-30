7ab770b8d92d2d579ee93a51df7ae5770516debf
Anthony1706024 <anthonypalma590@gmail.com>
2026-07-29 20:29:04 -0500

    Cambios1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1349,15 +1349,15 @@
                             </a>
                         </li>
                         <li>
-                            <a href="mailto:contacto@kapitalhaus.pe">
+                            <a href="mailto:contactos@kapitalhaus.pe">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
-                                contacto@kapitalhaus.pe
+                                contactos@kapitalhaus.pe
                             </a>
                         </li>
                         <li>
                             <a href="#">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 010-5 2.5 2.5 0 010 5z"/></svg>
-                                Lima, Perú
+                                Trujillo, Perú
                             </a>
                         </li>
                     </ul>
